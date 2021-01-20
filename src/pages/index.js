174ae7c964f9a6c11a2cd2e714313b3a87369d57
Marco Bianchi <marco.bianchi@deustechnology.com>
2021-01-20 22:08:27 +0100

    add time selection, modified incon select and added co2 results

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import classnames from 'classnames'
 
 import Select from 'react-select';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
 
 import { Layout } from "../components/layout"
 import { IconSelect } from "../components/icon-select"
@@ -21,71 +24,107 @@ const IndexPage = () => {
     ]
 
     const [selectedCountry, setSelectedCountry] = useState(null);
-    const [selectedConsumption, setSelectedConsumption] = useState(null);
-  
+    const [selectedConsumption, setSelectedConsumption] = useState('');
+    const [timeFrom, setTimeFrom] = useState(null);
+    const [timeTo, setTimeTo] = useState(null);
+    const [co2Produced, setCo2Produced] = useState(null);
+
+
+    useEffect(() => {
+      if (selectedCountry && selectedConsumption && timeFrom && timeTo) setCo2Produced(69);
+    }, [selectedCountry, selectedConsumption, timeFrom, timeTo])
+
     return (
-    <Layout contentClassName={style.index}>
-      <SEO title="Home" />
-      <section >
-        <h1>Welcome on Carbon Alt!</h1>
-        <p>On this platform we want to share awarness about the connection between carbon dioxide emissions and electricity consumption.
-          Would you like to know more about carbon dioxide effects?
-          Check our <a href="/facts">facts</a> section!
-        </p>
-
-        <p>
-        Start selecting your area! 
-        Check which will be the “greenest hour” of today, the hour when the electricity production will cause less emission,
-        and discover how much you would emit today using your household appliances!
-        </p>
-      </section>
-
-      <section>
-        <h1>CO&#8322; Usage Calculator</h1>
-
-        <div className={style.countrySelector}>
-          <label>Choose your country:</label>
-          <Select
-            onChange={(v) => setSelectedCountry(v.value)}
-            className={style.selector} options={countryOptions}
-          />
-        </div>
-        {
-          selectedCountry &&
-          <div className={style.bestTime}>
-            <p>
-              Today emissions derived from electricity production are at
-              the minimum at:
-            </p>
-            <span>{BEST_CO2_TIMES[selectedCountry]}</span>
-          </div>
-        }
-        <div className={style.applianceSelector}>
-          <p>
-            Pick your appliance:
+    <Layout>
+      <article>
+        <SEO title="Home" />
+        <section >
+          <h1>Welcome!</h1>
+          <p>On this platform we want to share awarness about the connection between carbon dioxide emissions and electricity consumption.
+            Would you like to know more about carbon dioxide effects?
+            Check our <a href="/facts">facts</a> section!
           </p>
 
-          <IconSelect options={[
-              // TODO move to config file
-              {name: 'washing-machine', consumption: 1.05},
-              {name: 'laptop', consumption: 0.30},
-              {name: 'hair-dryer', consumption: 0.84},
-              {name: 'mobile-phone', consumption: 0.06},
-              {name: 'laptop-charging', consumption: 0.60},
-              {name: 'dishwasher', consumption: 0.65},
-              {name: 'vacuum-cleaner', consumption: 1.50},
-            ]}
-            onChange={o => setSelectedConsumption(o.consumption)}/>
-            {
-              selectedConsumption &&
-              <>
-                <p>The appliance of your choice has an average consumption of [kWh]:</p>
-                <input type="number" value={selectedConsumption}></input>
-              </>
-            }
+          <p>
+          Start selecting your area! 
+          Check which will be the “greenest hour” of today, the hour when the electricity production will cause less emission,
+          and discover how much you would emit today using your household appliances!
+          </p>
+        </section>
 
-        </div>
-      </section>
+        <section>
+          
+          <h1>CO&#8322; Usage Calculator</h1>
+
+          <div className={style.countrySelector}>
+            <label>Choose your country:</label>
+            <Select
+              onChange={(v) => setSelectedCountry(v.value)}
+              className={style.selector} options={countryOptions}
+            />
+          </div>
+          {
+            selectedCountry &&
+            <div className={style.results}>
+              <p>
+                Today emissions derived from electricity production are at
+                the minimum at:
+              </p>
+              <span>{BEST_CO2_TIMES[selectedCountry]}</span>
+            </div>
+          }
+          <div className={style.applianceSelector}>
+
+            <p>Insert the average consumption of your appliance of your choice [kWh]:</p>
+
+            <input type="number" onChange={e => setSelectedConsumption(e.target.value)} value={selectedConsumption}></input>
+
+            <p>Or choose one of our predefined profiles:</p>
+
+            <IconSelect
+              selectedConsumption={selectedConsumption}
+              options={[
+                // TODO move to config file
+                {name: 'washing-machine', consumption: 1.05},
+                {name: 'laptop', consumption: 0.30},
+                {name: 'hair-dryer', consumption: 0.84},
+                {name: 'mobile-phone', consumption: 0.06},
+                {name: 'laptop-charging', consumption: 0.60},
+                {name: 'dishwasher', consumption: 0.65},
+                {name: 'vacuum-cleaner', consumption: 1.50},
+              ]}
+              onChange={o => setSelectedConsumption(o.consumption)}/>
+
+            <p>Select period of today usage:</p>
+
+            <div className={style.periodSelect}>
+              {/* TODO add validation */}
+              <div className={style.picker}>
+                <span>From:</span>
+                <TimePicker
+                  showSecond={false}
+                  onChange={t => setTimeFrom(t)}
+                />
+              </div>
+              <div className={style.picker}>
+                <span>To:</span>
+                <TimePicker
+                  showSecond={false}
+                  onChange={t => setTimeTo(t)}
+                />
+              </div>
+            </div>
+            
+            {
+              co2Produced &&
+              <div className={classnames(style.results, style.co2Results)}>
+                <p>The total amount of CO&#8322; emitted for the indicated consumption is:</p>
+                <span>{co2Produced} g</span>
+              </div>
+            }
+          </div>
+        </section>
+      </article>
     </Layout>
   );
 }
